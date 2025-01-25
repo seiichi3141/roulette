1d68287c16a6eeb56dc292dccebc4c701db20bfa
Seiichiro Tanaka <seiichi3141@gmail.com>
2025-01-25 16:22:47 +0900

    deploy

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "920317757"
+    serviceWorkerVersion: "987896198"
   }
 });
